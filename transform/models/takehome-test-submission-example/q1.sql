7476c6263a6a0a9615fcecc5908971799156e867
Jawad994 <170409802+Jawad994@users.noreply.github.com>
2025-01-11 21:16:10 +0800

    ADT-XXX-Jawad-Afzaal
    Jawad Afzaal

--- transform/models/takehome-test-submission-example/q1.sql
+++ transform/models/takehome-test-submission-example/q1.sql
@@ -4,8 +4,9 @@
     )
 }}
 
-WITH customers AS {
-    SELECT
-      customer_id,
-      ...
-}
+SELECT
+ country_name,
+ ROUND(SUM(gmv_local), 2) AS total_gmv
+FROM `dhh---analytics-apac.sandbox_ssc.orderscsc`
+GROUP BY country_name
+ORDER BY total_gmv DESC
